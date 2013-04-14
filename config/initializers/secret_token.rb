a24608311f0acb97afeb9dcb923d430aa26d03d6
Faisal Iqbal <faisal.learner@hotmail.com>
2013-04-15 02:11:52 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkedintest::Application.config.secret_token = '9cc9f2092de1038e3c103c0d93832f4ee47a8e4088874927bfd6a128a2acc9ae1af5982ca90b4583ced8a79c1891f75f0f6a5bfe5a911a09dadb372eb8c1e5f8'
